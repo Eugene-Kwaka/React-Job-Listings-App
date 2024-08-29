382bed852912cc9e619d5bcb303804eb15e3831c
Eugene Kwaka <kwakaeugene@gmail.com>
2024-08-29 17:05:35 -0500

    Enabled the AddJob functionality using the AddJob form. Used useState() for each field value in the form because they constantly change, hence updating state each time.

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,26 +1,87 @@
-import React from 'react'
+
+import { useState } from "react";
+
 
 const AddJobPage = () => {
+
+
+    // I create state for each field in the form(reps each job property e.g title, type, description...)
+    const [title, setTitle] = useState('');
+    const [type, setType] = useState('');
+    const [description, setDescription] = useState('');
+    const [location, setLocation] = useState('');
+    const [salary, setSalary] = useState('');
+    const [companyName, setCompanyName] = useState('');
+    const [companyDescription, setCompanyDescription] = useState('');
+    const [companyContactEmail, setCompanyContactEmail] = useState('');
+    const [companyContactPhone, setCompanyContactPhone] = useState('');
+
+    // I am creating my own submitForm functionality after adding field values to the form.
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        /* I'm creating an object that will contain all the added values from the field that will submit the data to the server/API*/
+        const newJob = {
+            title, 
+            type,
+            location,
+            description,
+            salary,
+            company:{
+                name: companyName,
+                description: companyDescription,
+                contactEmail: companyContactEmail,
+                contactPhone: companyContactPhone
+
+            },
+        };
+
+        console.log(newJob);
+    }
+
+
   return (
     <>
     
-    <section class="bg-indigo-50">
-      <div class="container m-auto max-w-2xl py-24">
+    <section className="bg-indigo-50">
+      <div className="container m-auto max-w-2xl py-24">
         <div
-          class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
+          className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
-          <form>
-            <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
+            {/* I have created my own submitForm() function that handles the submission of the Form that reads the onSubmit() eventHandler.*/}
+          <form onSubmit={submitForm}>
+            <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
-            <div class="mb-4">
-              <label for="type" class="block text-gray-700 font-bold mb-2"
+            <div className="mb-4">
+              <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
                 >Job Type</label
               >
               <select
                 id="type"
                 name="type"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 required
+                
+
+                /**
+                 * These two variables ccreate a Controlled Component.
+                 * A Controlled Component are those where form data is handled by the React component's state. 
+                 * They also ensure state synchronization by ensuring the <select> element and the state variable(job.type) are always synced.
+                 * Also allows for easy validation and additional logic to be added on the onChange() handler if needed. 
+                 */
+
+                /**
+                 * This line sets the current value of the <select> element to the value of the 'job.type' property from our job object.
+                 * It ensures the <select> element reflects the current state of the type property. */ 
+                value={type}
+                
+                /**
+                 * This line sets up an event handler for the onChange() event of the <select> element. 
+                 * When a user selects a different job type, the event object (e) is triggered, which calls the setType(e.target.value).
+                 * It then updates the state variable with the new value.
+                 * The 'target.value' is whatever is selected and targeted in the <select> field. 
+                 */
+                onChange={(e) => setType(e.target.value)}
               >
                 <option value="Full-Time">Full-Time</option>
                 <option value="Part-Time">Part-Time</option>
@@ -29,43 +90,49 @@ const AddJobPage = () => {
               </select>
             </div>
 
-            <div class="mb-4">
-              <label class="block text-gray-700 font-bold mb-2"
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2"
                 >Job Listing Name</label
               >
               <input
                 type="text"
                 id="title"
                 name="title"
-                class="border rounded w-full py-2 px-3 mb-2"
-                placeholder="eg. Beautiful Apartment In Miami"
+                className="border rounded w-full py-2 px-3 mb-2"
+                placeholder="eg. Junior Front-end Engineer"
                 required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </div>
-            <div class="mb-4">
+            <div className="mb-4">
               <label
-                for="description"
-                class="block text-gray-700 font-bold mb-2"
+                htmlFor="description"
+                className="block text-gray-700 font-bold mb-2"
                 >Description</label
               >
               <textarea
                 id="description"
                 name="description"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="Add any job duties, expectations, requirements, etc"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               ></textarea>
             </div>
 
-            <div class="mb-4">
-              <label for="type" class="block text-gray-700 font-bold mb-2"
+            <div className="mb-4">
+              <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
                 >Salary</label
               >
               <select
                 id="salary"
                 name="salary"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 required
+                value={salary}
+                onChange={(e) => setSalary(e.target.value)}
               >
                 <option value="Under $50K">Under $50K</option>
                 <option value="$50K - 60K">$50K - $60K</option>
@@ -81,83 +148,93 @@ const AddJobPage = () => {
               </select>
             </div>
 
-            <div class='mb-4'>
-              <label class='block text-gray-700 font-bold mb-2'>
+            <div className='mb-4'>
+              <label className='block text-gray-700 font-bold mb-2'>
                 Location
               </label>
               <input
                 type='text'
                 id='location'
                 name='location'
-                class='border rounded w-full py-2 px-3 mb-2'
+                className='border rounded w-full py-2 px-3 mb-2'
                 placeholder='Company Location'
-                required           
+                required  
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}         
               />
             </div>
 
-            <h3 class="text-2xl mb-5">Company Info</h3>
+            <h3 className="text-2xl mb-5">Company Info</h3>
 
-            <div class="mb-4">
-              <label for="company" class="block text-gray-700 font-bold mb-2"
+            <div className="mb-4">
+              <label htmlFor="company" className="block text-gray-700 font-bold mb-2"
                 >Company Name</label
               >
               <input
                 type="text"
                 id="company"
                 name="company"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 placeholder="Company Name"
+                value={companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
               />
             </div>
 
-            <div class="mb-4">
+            <div className="mb-4">
               <label
-                for="company_description"
-                class="block text-gray-700 font-bold mb-2"
+                htmlFor="company_description"
+                className="block text-gray-700 font-bold mb-2"
                 >Company Description</label
               >
               <textarea
                 id="company_description"
                 name="company_description"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 rows="4"
                 placeholder="What does your company do?"
+                value={companyDescription}
+                onChange={(e) => setCompanyDescription(e.target.value)}
               ></textarea>
             </div>
 
-            <div class="mb-4">
+            <div className="mb-4">
               <label
-                for="contact_email"
-                class="block text-gray-700 font-bold mb-2"
+                htmlFor="contact_email"
+                className="block text-gray-700 font-bold mb-2"
                 >Contact Email</label
               >
               <input
                 type="email"
                 id="contact_email"
                 name="contact_email"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 placeholder="Email address for applicants"
                 required
+                value={companyContactEmail}
+                onChange={(e) => setCompanyContactEmail(e.target.value)}
               />
             </div>
-            <div class="mb-4">
+            <div className="mb-4">
               <label
-                for="contact_phone"
-                class="block text-gray-700 font-bold mb-2"
+                htmlFor="contact_phone"
+                className="block text-gray-700 font-bold mb-2"
                 >Contact Phone</label
               >
               <input
                 type="tel"
                 id="contact_phone"
                 name="contact_phone"
-                class="border rounded w-full py-2 px-3"
+                className="border rounded w-full py-2 px-3"
                 placeholder="Optional phone for applicants"
+                value={companyContactPhone}
+                onChange={(e) => setCompanyContactPhone(e.target.value)}
               />
             </div>
 
             <div>
               <button
-                class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+                className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
                 Add Job
